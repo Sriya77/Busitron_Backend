6ba7e1c9908f5b9c937a6631571cdb33eafa0187
busitrondevproject <maheshbalugiri@busitron.com>
2025-02-26 02:11:46 +0530

    optimize get and update controller for company setting

--- controller/companySetting.controller.js
+++ controller/companySetting.controller.js
@@ -2,24 +2,27 @@ import { companySettingModel } from "../models/companySetting.model.js";
 import { asyncHandler } from "../utils/asyncHandle.js";
 import { errorHandler } from "../utils/errorHandle.js";
 import { apiResponse } from "../utils/apiResponse.js";
-import mongoose from "mongoose";
 
-export const companySettings = asyncHandler(async (req, res) => {
+export const updateCompanySettings = asyncHandler(async (req, res) => {
     try {
-        const { companyName, companyEmail, phoneNumber, website } = req.body;
+        const { id, companyName, companyEmail, phoneNumber, website } = req.body;
 
         if (!companyName || !companyEmail || !phoneNumber || !website) {
             throw new errorHandler(400, "All fields are required");
         }
 
-        const companySetting = await companySettingModel.create({
-            companyName,
-            companyEmail,
-            phoneNumber,
-            website,
-        });
-        res.status(201).json(
-            new apiResponse(201, companySetting, "company setting created successfully")
+        const updatedCompany = await companySettingModel.findOneAndUpdate(
+            { _id: id },
+            { companyName, companyEmail, phoneNumber, website },
+            { new: true, runValidators: true }
+        );
+
+        if (!updatedCompany) {
+            throw new errorHandler(404, "Company settings not found");
+        }
+
+        res.status(200).json(
+            new apiResponse(200, updatedCompany, "Company settings updated successfully")
         );
     } catch (err) {
         throw new errorHandler(500, err.message);
