b1fef2b4dcc879930fe61336540b4962a89633a1
lokamcharan2703 <l.charan@busitron.com>
2025-03-06 12:30:26 +0530

    add estimate

--- controller/estimate.controller.js
+++ controller/estimate.controller.js
@@ -3,78 +3,112 @@ import { uploadToS3 } from "../services/aws.service.js";
 import { apiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandle.js";
 import { errorHandler } from "../utils/errorHandle.js";
-
+import { User } from "../models/user.models.js";
+import { sendEstimateEmail } from "../helper/sendEstimateEmail.helper.js";
 export const createEstimate = asyncHandler(async (req, res, next) => {
 	try {
 		const {
 			estimateNumber,
 			validTill,
 			currency,
-			client,
-			taxCalculation,
-			description,
+			clientId,
+			userId,
 			status,
-			products,
-			summary,
+			productName,
 			responseMessage,
 		} = req.body;
-
-		if (!estimateNumber || !client) {
-			new errorHandler("Estimate Number and Client are required.", 400);
-		}
-
-		const existingEstimate = await Estimate.findOne({ estimateNumber });
-
-		if (existingEstimate) {
-			new errorHandler(
-				"An estimate with this number already exists.",
-				400
+		const products = JSON.parse(req.body.products);
+		const summary = JSON.parse(req.body.summary);
+		if (!estimateNumber || !clientId) {
+			throw new errorHandler(
+				400,
+				"Estimate Number, Client, and Client Email are required."
 			);
 		}
-
+		const existingEstimate = await Estimate.findOne({ estimateNumber });
+		if (existingEstimate) {
+			throw new errorHandler(
+				400,
+				"An estimate with this number already exists."
+			);
+		}
+		let findUser = await User.findById({ _id: clientId });
+		if (!findUser) {
+			throw new errorHandler(400, "User Not Found");
+		}
+		let storeUploadResult = [];
 		const estimate = new Estimate({
 			estimateNumber,
 			validTill,
 			currency,
-			client,
-			taxCalculation,
-			description,
+			clientId,
+			userId,
 			status,
+			productName,
 			products,
 			summary,
 			responseMessage,
 		});
 		const savedEstimate = await estimate.save();
 		if (req.files?.length) {
-			estimate.uploadedFile = await Promise.all(
-				req.files.map((file) =>
-					uploadToS3(file, `Estimates/${estimate._id}/attachments`)
-				)
-			);
+			for (let file of req.files) {
+				let uploadResult = await uploadToS3(
+					file,
+					`Estimates/${estimate._id}/attachments`
+				);
+				storeUploadResult.push(uploadResult);
+			}
 		}
-
+		savedEstimate.uploadedFile = storeUploadResult;
+		await savedEstimate.save();
+		const emailSubject = `New Estimate Created: ${estimateNumber}`;
+		const emailMessage = `
+            <p>Dear ${findUser.name},</p>
+            <p>Your estimate number, <strong>${estimateNumber}</strong> has been created.</p>
+            <p>Details:</p>
+            <ul>
+                <li><strong>Valid Till:</strong> ${validTill}</li>
+                <li><strong>Currency:</strong> ${currency}</li>
+                <li><strong>Final Amount:</strong> ${summary.finalAmount}</li>
+                <li><strong>Description:</strong> ${
+					products.itemDescription || "No Description Given"
+				}</li>
+            </ul>
+        `;
+		await sendEstimateEmail(
+			findUser.email,
+			emailSubject,
+			emailMessage,
+			savedEstimate._id,
+			true
+		);
 		return res
 			.status(201)
 			.json(
 				new apiResponse(
 					201,
 					savedEstimate,
-					"Estimate created successfully."
+					"Estimate Created successfully. Please Check Email"
 				)
 			);
+		// return res.status(201).json({message : "Hello world", success : true})
 	} catch (error) {
-		new errorHandler(error.message || "Internal Server Error", 500);
+		throw new errorHandler(error.message || 500, "Internal Server Error");
 	}
 });
-
 export const getAllEstimates = asyncHandler(async (req, res, next) => {
 	try {
-		const estimates = await Estimate.find().populate("client");
+		const estimates = await Estimate.find()
+			.populate({
+				path: "clientId",
+				select: "name",
+			})
+			.populate({ path: "userId", select: "name" })
+			.sort({ createdAt: -1 });
 		return res
 			.status(200)
 			.json(
 				new apiResponse(
-					true,
 					200,
 					estimates,
 					"All estimates retrieved successfully."
@@ -84,20 +118,16 @@ export const getAllEstimates = asyncHandler(async (req, res, next) => {
 		new errorHandler(error.message || "Internal Server Error", 500);
 	}
 });
-
 export const getEstimateById = asyncHandler(async (req, res, next) => {
 	try {
 		const { id } = req.params;
-
 		if (!id) {
 			new errorHandler("Invalid estimate ID.", 400);
 		}
-
-		const estimate = await Estimate.findById(id).populate("client");
+		const estimate = await Estimate.findById(id);
 		if (!estimate) {
 			new errorHandler("Estimate not found.", 404);
 		}
-
 		return res
 			.status(200)
 			.json(
@@ -112,22 +142,18 @@ export const getEstimateById = asyncHandler(async (req, res, next) => {
 		new errorHandler(error.message || "Internal Server Error", 500);
 	}
 });
-
 export const updateEstimate = asyncHandler(async (req, res, next) => {
 	try {
 		const { id } = req.params;
-
 		if (!id) {
 			new errorHandler("Invalid estimate ID.", 400);
 		}
-
 		const updatedEstimate = await Estimate.findByIdAndUpdate(id, req.body, {
 			new: true,
 		});
 		if (!updatedEstimate) {
 			new errorHandler("Estimate not found.", 404);
 		}
-
 		return res
 			.status(200)
 			.json(
@@ -142,23 +168,35 @@ export const updateEstimate = asyncHandler(async (req, res, next) => {
 		new errorHandler(error.message || "Internal Server Error", 500);
 	}
 });
-
 export const deleteEstimate = asyncHandler(async (req, res, next) => {
 	try {
 		const { id } = req.params;
-
 		if (!id) {
-			new errorHandler("Invalid estimate ID.", 400);
+			new errorHandler(400, "Invalid estimate ID");
 		}
+		let deletedEstimate = await Estimate.findByIdAndDelete(id).populate({
+			path: "clientId",
+			select: "name email",
+		});
 
-		const deletedEstimate = await Estimate.findByIdAndDelete(id);
 		if (!deletedEstimate) {
-			new errorHandler("Estimate not found.", 404);
+			new errorHandler(400, "Estimate not found.");
 		}
-
+		const emailSubject = `Estimate Deleted: ${deletedEstimate.estimateNumber}`;
+		const emailMessage = `
+            <p>Dear ${deletedEstimate.clientId.name},</p>
+            <p>Your estimate number, <strong>${deletedEstimate.estimateNumber}</strong> has been deleted.</p>
+        `;
+		await sendEstimateEmail(
+			deletedEstimate.clientId.email,
+			emailSubject,
+			emailMessage,
+			"",
+			false
+		);
 		return res
 			.status(200)
-			.json(new apiResponse(true, 200, "Estimate deleted successfully."));
+			.json(new apiResponse(200, "", "Record Deleted Successfully."));
 	} catch (error) {
 		new errorHandler(error.message || "Internal Server Error", 500);
 	}
--- models/estimate.model.js
+++ models/estimate.model.js
@@ -1,89 +1,91 @@
 import mongoose from "mongoose";
 import { v4 } from "uuid";
 
-const EstimateSchema = new mongoose.Schema(
-	{
-		estimateNumber: {
-			type: String,
-			required: false,
-			unique: false,
-			default: () => `EST-${v4().split("-")[0]}`,
-		},
-		validTill: {
-			type: Date,
-			required: false,
-		},
-		currency: {
-			type: String,
-			required: false,
-			enum: ["INR", "USD"],
-		},
-		client: {
-			type: String,
-			required: false,
-		},
-		taxCalculation: {
-			type: String,
-			enum: ["Before Discount", "After Discount", "None"],
-			required: false,
-		},
-		description: { type: String },
-		status: {
-			type: String,
-			enum: ["Completed", "In progress", "Pending", "To do"],
-			required: false,
-		},
-		products: [
-			{
-				description: {
-					type: String,
-					required: false,
-				},
-				quantity: {
-					type: Number,
-					required: false,
-				},
-				unitPrice: {
-					type: Number,
-					required: false,
-				},
-				tax: {
-					type: String,
-					required: false,
-				},
-				amount: {
-					type: Number,
-					required: false,
-				},
-			},
-		],
-		summary: {
-			subTotal: {
-				type: Number,
-				required: false,
-			},
-			discount: {
-				type: Number,
-			},
-			tax: {
-				type: Number,
-			},
-			total: {
-				type: Number,
-				required: false,
-			},
-		},
-		responseMessage: {
-			type: String,
-		},
-		uploadedFile: [
-			{
-				type: String,
-				required: false,
-			},
-		],
-	},
-	{ timestamps: true }
-);
 
+const EstimateSchema = new mongoose.Schema(
+    {
+        estimateNumber: {
+            type: String,
+            required: false,
+            unique: false,
+            default: () => `EST-${v4().split("-")[0]}`,
+        },
+        validTill: {
+            type: String,
+            required: false,
+        },
+        currency: {
+            type: String,
+            required: false,
+            enum: ["INR", "USD"],
+        },
+        clientId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        userId : {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        status: {
+            type: String,
+            enum: ["Completed", "In progress", "Pending", "To do"],
+            required: false,
+            default : "Pending"
+        },
+        productName : {
+            type : String,
+        },
+        products: {
+            itemName: {
+                type: String,
+                required: false,
+            },
+            itemDescription : {
+                type: String,
+                required: false,
+            },
+            quantity: {
+                type: String,
+                required: false,
+            },
+            unitPrice: {
+                type: String,
+                required: false,
+            },
+            taxPercentage: {
+                type: String,
+                required: false,
+            },
+            amount: {
+                type: String,
+                required: false,
+            },
+        },
+        summary: {
+            subTotal: {
+                type: String,
+            },
+            discount: {
+                type: String,
+            },
+            taxAmount: {
+                type: String,
+            },
+            finalAmount: {
+                type: String,
+            },
+        },
+        responseMessage: {
+            type: String,
+        },
+        uploadedFile: [
+            {
+                type: String,
+                required: false,
+            },
+        ],
+    },
+    { timestamps: true }
+);
 export const Estimate = mongoose.model("Estimate", EstimateSchema);
