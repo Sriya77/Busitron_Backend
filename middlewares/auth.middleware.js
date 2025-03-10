1a6eacd4379436525e2bd39fbbf39c78b7d0eeb5
busitrondevproject <maheshbalugiri@busitron.com>
2025-03-10 16:37:52 +0530

    docker changes

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -14,7 +14,6 @@ export const authenticateUser = asyncHandler(async (req, res, next) => {
             }
             token = newAccessToken;
         }
-
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
